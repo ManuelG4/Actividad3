0c90232ca1f2e53f204b1d680da20434d927cc73
ManuelG4 <pgalindoh@miumg.edu.gt>
2023-02-28 18:59:54 -0600

    primer_commit

---
+++ Ifternario.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+main(){
+int n1=0,n2=0,n3=0;	
+float promedio =0;
+string mensaje="";
+cout<<"Ingrese Nota1:";
+cin>>n1;
+cout<<"Ingrese Nota2:";
+cin>>n2;
+cout<<"Ingrese Nota3:";
+cin>>n3;
+promedio = (n1+n2+n3)/3;
+mensaje = promedio >60 ?"Aprabado" : "Reprobado";
+cout<<"El Promedio es: "<<promedio<<" Por lo tanto esta "<<mensaje<<endl;
+
+if(promedio>=0 && promedio<61){
+	cout<<"Mala nota";
+}
+
+if(promedio>=61 && promedio<81){
+	cout<<"Buena nota";
+}
+
+if(promedio>=81){
+	cout<<"Excelente nota";
+}
+
+system("pause");	
+}
